fcbc02d34756023967ca557493198b12ceaa66d4
kuaars <kutkovaleksej5@gmail.com>
2026-03-17 23:04:35 +0300

    Update install_context_menu.ps1

--- install_context_menu.ps1
+++ install_context_menu.ps1
@@ -7,17 +7,24 @@ if (-not (Test-Path -LiteralPath $scriptPath)) {
   throw "File not found: $scriptPath"
 }
 
-$menuKey = "HKCU:\Software\Classes\*\shell\SecureDeleteOverwrite"
-$cmdKey = Join-Path $menuKey "command"
+$subKey = "Software\Classes\*\shell\SecureDeleteOverwrite"
+$cmdSubKey = "$subKey\command"
 
-New-Item -Path $menuKey -Force | Out-Null
-Set-ItemProperty -Path $menuKey -Name "(default)" -Value "Удалить с перезаписью"
-Set-ItemProperty -Path $menuKey -Name "Icon" -Value "imageres.dll,-5302"
-Set-ItemProperty -Path $menuKey -Name "MultiSelectModel" -Value "Player"
+$k = [Microsoft.Win32.Registry]::CurrentUser.CreateSubKey($subKey)
+$caption = [string]::new([char[]]@(
+  0x0423,0x0434,0x0430,0x043B,0x0438,0x0442,0x044C,0x0020,
+  0x0441,0x0020,
+  0x043F,0x0435,0x0440,0x0435,0x0437,0x0430,0x043F,0x0438,0x0441,0x044C,0x044E
+))
+$k.SetValue("", $caption, [Microsoft.Win32.RegistryValueKind]::String)
+$k.SetValue("Icon", "imageres.dll,-5302", [Microsoft.Win32.RegistryValueKind]::String)
+$k.SetValue("MultiSelectModel", "Player", [Microsoft.Win32.RegistryValueKind]::String)
+$k.Close()
 
-New-Item -Path $cmdKey -Force | Out-Null
+$ck = [Microsoft.Win32.Registry]::CurrentUser.CreateSubKey($cmdSubKey)
 $cmd = "cmd.exe /c `"python `"$scriptPath`" `"%1`"`""
-Set-ItemProperty -Path $cmdKey -Name "(default)" -Value $cmd
+$ck.SetValue("", $cmd, [Microsoft.Win32.RegistryValueKind]::String)
+$ck.Close()
 
 Write-Host "Done. Context menu item installed for current user."
 
